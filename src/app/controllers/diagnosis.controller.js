ad341d1c90f8d7e1d80d4570bc83db73fafce71e
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2022-06-01 21:28:07 +0700

    Update API Diagnosis

--- src/app/controllers/diagnosis.controller.js
+++ src/app/controllers/diagnosis.controller.js
@@ -3,63 +3,63 @@ const validateUtils = require('../utils/validate.util');
 const MESSAGE = require('../../constants/messages.constant');
 
 class DiagnosisController {
-  // [GET] /api/diagnosis
-  async index(req, res) {}
+    // [GET] /api/diagnosis
+    async index(req, res) {}
 
-  // [GET] /api/diagnosis/:id
-  async show(req, res) {
-    try {
-      if (!validateUtils.validateObjectId(req.params.id)) {
-        res.statusMessage = MESSAGE.DIAGNOSIS_NOT_FOUND;
-        return res.sendStatus(404);
-      }
+    // [GET] /api/diagnosis/:id
+    async show(req, res) {
+        try {
+            if (!validateUtils.validateObjectId(req.params.id)) {
+                res.statusMessage = MESSAGE.DIAGNOSIS_NOT_FOUND;
+                return res.sendStatus(404);
+            }
 
-      const diagnosis = await diagnosisService.getById(req.params.id);
-      res.json(diagnosis);
-    } catch (e) {
-      res.statusMessage = MESSAGE.SERVER_ERROR;
-      res.sendStatus(500);
+            const diagnosis = await diagnosisService.getById(req.params.id);
+            res.json(diagnosis);
+        } catch (e) {
+            res.statusMessage = MESSAGE.SERVER_ERROR;
+            res.sendStatus(500);
+        }
     }
-  }
 
-  // [GET] /api/diagnosis/user
-  async diagnosisUser(req, res) {
-    try {
-      const diagnosis = await diagnosisService.getByUserId(req.user.id);
-      res.json(diagnosis);
-    } catch (e) {
-      res.statusMessage = MESSAGE.SERVER_ERROR;
-      res.sendStatus(500);
+    // [GET] /api/diagnosis/user
+    async diagnosisUser(req, res) {
+        try {
+            const diagnosis = await diagnosisService.getByUserId(req.user.id);
+            res.json(diagnosis);
+        } catch (e) {
+            res.statusMessage = MESSAGE.SERVER_ERROR;
+            res.sendStatus(500);
+        }
     }
-  }
 
-  // [POST] /api/diagnosis
-  async create(req, res) {
-    try {
-      console.log(req.body);
-      if (!req.body.user || !validateUtils.validateObjectId(req.body.user)) {
-        res.statusMessage = MESSAGE.USER_NOT_FOUND;
-        return res.sendStatus(400);
-      }
+    // [POST] /api/diagnosis
+    async create(req, res) {
+        try {
+            console.log(req.body);
+            if (!req.body.user || !validateUtils.validateObjectId(req.body.user)) {
+                res.statusMessage = MESSAGE.USER_NOT_FOUND;
+                return res.sendStatus(400);
+            }
 
-      if (!req.body.title) {
-        res.statusMessage = MESSAGE.DIAGNOSIS_NOT_EMPTY;
-        return res.sendStatus(400);
-      }
+            if (!req.body.predicted_results) {
+                res.statusMessage = MESSAGE.DIAGNOSIS_NOT_EMPTY;
+                return res.sendStatus(400);
+            }
 
-      if (!req.body.symtomps) {
-        res.statusMessage = MESSAGE.SYMPTOMS_NOT_EMPTY;
-        return res.sendStatus(400);
-      }
+            if (!req.body.symtomps) {
+                res.statusMessage = MESSAGE.SYMPTOMS_NOT_EMPTY;
+                return res.sendStatus(400);
+            }
 
-      const diagnosis = await diagnosisService.createOne(req.body);
-      res.json(diagnosis);
-    } catch (e) {
-      console.log(e);
-      res.statusMessage = MESSAGE.SERVER_ERROR;
-      res.sendStatus(500);
+            const diagnosis = await diagnosisService.createOne(req.body);
+            res.json(diagnosis);
+        } catch (e) {
+            console.log(e);
+            res.statusMessage = MESSAGE.SERVER_ERROR;
+            res.sendStatus(500);
+        }
     }
-  }
 }
 
 module.exports = new DiagnosisController();
--- src/app/models/schemas/diagnosis.schema.js
+++ src/app/models/schemas/diagnosis.schema.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const diagnosisSchema = new Schema(
-  {
+const diagnosisSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User', autopopulate: true },
     symtoms: { type: String },
-    results: { type: Array }
-  },
-  {
+    predicted_results: { type: Array }
+}, {
     timestamps: { createdAt: 'created_at' },
-  }
-);
+});
 
 module.exports = diagnosisSchema;
